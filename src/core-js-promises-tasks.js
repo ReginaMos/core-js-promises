695a0855a82b5d507c4444e72f155a8f7224cf60
Regina Moiseeva <moiseevaryu@gmail.com>
2025-04-08 11:21:29 +0500

    feat: add last solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -131,8 +131,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((acc, cur) => {
+    return acc.then((res) => {
+      return cur.then((value) => res + value);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
